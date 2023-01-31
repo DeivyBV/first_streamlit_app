4936268e27405319df6536bf3a2e028daccbd112
Deivy Barquero Vargas <42472996+DeivyBV@users.noreply.github.com>
2023-01-31 12:27:24 -0600

    no kiwi default

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fuityvice api response
 streamlit.header('Fuityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      streamlit.error('Please select a fruit to get information.')
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+   streamlit.error()
+
 streamlit.write('The user entered',fruit_choice)
 
-#Calling Fruityvice API from Streamlit App!
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# Transforms the json format to a dataframe format 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displays the df into the page
-streamlit.dataframe(fruityvice_normalized)
+
 
 # dont run anything past here while we troubleshoot
 streamlit.stop()
