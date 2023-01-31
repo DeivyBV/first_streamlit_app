9b2545674a3a4d35e38be5978db8e7152a644fa9
Deivy Barquero Vargas <42472996+DeivyBV@users.noreply.github.com>
2023-01-31 12:47:19 -0600

    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,3 @@ if steamlit.button('Get Fruit Load List'):
 # dont run anything past here while we troubleshoot
 streamlit.stop()
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.text('Thanks for adding ' + add_my_fruit)
-
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
